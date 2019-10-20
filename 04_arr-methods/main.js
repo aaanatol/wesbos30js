2dab59a2e28ee865c07b2180cbeb12722d4d9359
Anatol <ashkovtolik@gmail.com>
2019-10-20 22:44:20 +0300

    add less4

--- 04_arr-methods/main.js
+++ 04_arr-methods/main.js
@@ -35,13 +35,62 @@ document.addEventListener('DOMContentLoaded', () => {
 
 	// Array.prototype.reduce()
 	// 4. How many years did all the inventors live?
+	
+	const totalYearsLived = inventors.reduce((total, inventor) => {
+		return total + (inventor.passed - inventor.year);
+	}, 0);
+
+	console.table(totalYearsLived);
+
 	// 5. Sort the inventors by years lived
+
+	const orderedLived = inventors.sort(compareByYearsLived);
+
+	function compareByYearsLived(a, b) {
+		return ((a.passed - a.year) > (b.passed - b.year)) ? -1 : 1;
+	}
+
+	console.table(orderedLived);
+	
 	// 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
 	// https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
+	
+	// const category = document.querySelector('.mw-category');
+	// const links = [...category.querySelectorAll('a')];
+
+	// const de = links
+	// 							.map(link => link.textContent)
+	// 							.filter(streetName => streetName.includes('de'));
+
 	// 7. sort Exercise
 	// Sort the people alphabetically by last name
+
+	const alpha = people.sort(function(lastOne, nextOne) {
+		const [aLast, aFirst] = lastOne.split(', ');
+		const [bLast, bFirst] = nextOne.split(', ');
+
+		return aLast < bLast ? -1 : 1;
+	});
+
+	const  peopleJoined = people.map(man => man.replace(',', ''));
+
+	console.log(alpha);
+	console.table(peopleJoined);
+
 	// 8. Reduce Exercise
 	// Sum up the instances of each of these
 	const data = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck' ];
+
+	const instances = data.reduce(function(obj, item) {
+		if (!obj[item]) {
+			obj[item] = 0;
+		}
+		
+		obj[item]++;
+
+		return obj;
+	}, {});
+
+	console.log(instances);
 
 }); 
